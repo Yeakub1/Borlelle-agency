9253b408faaea88f525e76354efe92c9ee52ec87
Yeakub1 <mryeakub10@gmail.com>
2023-06-23 18:06:54 +0600

    vercel link

--- src/Authentication/Login/Login.jsx
+++ src/Authentication/Login/Login.jsx
@@ -56,7 +56,7 @@ const Login = () => {
           name: loginUser.displayName,
           email: loginUser.email,
         };
-        fetch("http://localhost:5000/users", {
+        fetch("https://agency-server-ten.vercel.app/users", {
           method: "POST",
           headers: {
             "content-type": "application/json",
